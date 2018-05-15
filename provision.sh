59b89bb4f71d87ddb01b45cfad67251fb8485058
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-05-16 02:28:48 +0300

    use ansble from PPA

--- provision.sh
+++ provision.sh
@@ -5,9 +5,11 @@ set -e
 sudo apt-get update
 echo "======================apt-get update done.=================="
 sudo apt-get -y upgrade
-sudo apt-get install -y python-dev python-pip
-pip install ansible
-sudo timedatectl set-timezone Europe/Istanbul
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+sudo timedatectl set-timezone Africa/Nairobi
 sudo localectl set-locale LANG=en_US.utf8
 git clone https://github.com/philophilo/packer_example.git
 cd packer_example
