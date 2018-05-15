7fec114345b02fca48ad23294a26b6f585832df2
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-05-16 01:58:32 +0300

    Allow cloning the repo

--- provision.sh
+++ provision.sh
@@ -10,5 +10,7 @@ pip install ansible
 sudo timedatectl set-timezone Europe/Istanbul
 sudo localectl set-locale LANG=en_US.utf8
 sudo wget 'https://s3.amazonaws.com/packeramidemo/i_playbook.yml'
+git clone https://github.com/philophilo/packer_example.git
+cd packer_example
 echo "=========Running build.========"
-sudo ansible-playbook i_playbook.yml
+sudo ansible-playbook playbook.yml
