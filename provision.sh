f7e0e6e7f7cd0fd895380d645850427111aacb34
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-05-16 01:41:05 +0300

    Add provisioner

---
+++ provision.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+set -e
+#provision.sh
+
+sudo apt-get update
+echo "======================apt-get update done.=================="
+sudo apt-get -y upgrade
+sudo apt-get install -y python-dev python-pip
+pip install ansible
+sudo timedatectl set-timezone Europe/Istanbul
+sudo localectl set-locale LANG=en_US.utf8
+sudo wget 'https://s3.amazonaws.com/packeramidemo/i_playbook.yml'
+echo "=========Running build.========"
+sudo ansible-playbook i_playbook.yml
